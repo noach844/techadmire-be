a1fa02480590605543fafe6e4bbc469477c4f75b
noach844 <68166931+noach844@users.noreply.github.com>
2023-10-25 20:43:22 +0300

    init

---
+++ src/test/java/com/example/techadmirebe/TechAdmireBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techadmirebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAdmireBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
